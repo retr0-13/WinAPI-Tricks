bd23b8d769645bc61291e255b5b307bfd2636a1e
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-11-12 23:48:34 -0600

    Update IsNvidiaGraphicsCardPresent.cpp

--- Misc/IsNvidiaGraphicsCardPresent.cpp
+++ Misc/IsNvidiaGraphicsCardPresent.cpp
@@ -3,13 +3,13 @@ BOOL IsNvidiaGraphicsCardPresent(VOID)
     DISPLAY_DEVICEW DisplayDevice; RtlZeroMemory(&DisplayDevice, sizeof(DISPLAY_DEVICEW));
     DisplayDevice.cb = sizeof(DISPLAY_DEVICEW);
 
-	DWORD dwDeviceId = ERROR_SUCCESS;
+    DWORD dwDeviceId = ERROR_SUCCESS;
 
-	while (EnumDisplayDevicesW(NULL, dwDeviceId, &DisplayDevice, 0))
-	{
-		if (StringFindSubstringW(DisplayDevice.DeviceString, (PWCHAR)L"NVIDIA") != NULL)
-			return TRUE;
-	}
+    while (EnumDisplayDevicesW(NULL, dwDeviceId, &DisplayDevice, 0))
+    {
+         if (StringFindSubstringW(DisplayDevice.DeviceString, (PWCHAR)L"NVIDIA") != NULL)
+	      return TRUE;
+    }
 
-	return FALSE;
+    return FALSE;
 }
