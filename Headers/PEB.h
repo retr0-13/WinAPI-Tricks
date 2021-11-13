5d89d593612bdb5aff3cb833c6d834c4f8f0bb4d
vxunderground <57078196+vxunderground@users.noreply.github.com>
2021-11-12 22:37:46 -0600

    Corrections
    N/A

--- Headers/PEB.h
+++ Headers/PEB.h
@@ -1,3 +1,9 @@
+typedef struct _LSA_UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} LSA_UNICODE_STRING, * PLSA_UNICODE_STRING, UNICODE_STRING, * PUNICODE_STRING;
+
 typedef struct _LDR_MODULE {
 	LIST_ENTRY              InLoadOrderModuleList;
 	LIST_ENTRY              InMemoryOrderModuleList;
